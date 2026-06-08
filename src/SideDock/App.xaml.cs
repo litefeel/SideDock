9f93579cfcee7a0e25fd53465997a38536be65f2
litefeel <litefeel@gmail.com>
2026-06-08 18:08:55 +0800

    Enforce single-instance startup

--- src/SideDock/App.xaml.cs
+++ src/SideDock/App.xaml.cs
@@ -4,8 +4,19 @@ namespace SideDock;
 
 public partial class App : Application
 {
+    private const string SingleInstanceMutexName = "Local\\SideDock.SingleInstance";
+
+    private Mutex? _singleInstanceMutex;
+
     protected override void OnStartup(StartupEventArgs e)
     {
+        _singleInstanceMutex = new Mutex(initiallyOwned: true, SingleInstanceMutexName, out var ownsMutex);
+        if (!ownsMutex)
+        {
+            Shutdown();
+            return;
+        }
+
         base.OnStartup(e);
         DispatcherUnhandledException += (_, args) =>
         {
@@ -16,5 +27,17 @@ public partial class App : Application
                 MessageBoxImage.Error);
             args.Handled = true;
         };
+
+        MainWindow = new MainWindow();
+        MainWindow.Show();
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _singleInstanceMutex?.ReleaseMutex();
+        _singleInstanceMutex?.Dispose();
+        _singleInstanceMutex = null;
+
+        base.OnExit(e);
     }
 }
